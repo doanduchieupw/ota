a96a5c367d8b9f7bae931bd80c03fac17909f4c4
doanduchieupw <doanduchieu.pw12@gmail.com>
2022-09-05 23:02:56 +0700

    first commit

--- index.js
+++ index.js
@@ -1,15 +1,33 @@
 const express = require('express');
-
 const app = express();
+const http = require('http');
+const server = http.createServer(app);
+const { Server } = require('socket.io');
+const io = new Server(server);
 const router = express.Router();
 
 router.get('/', (req, res) => {
     res.send('Hello World!');
 });
 
+router.post('/api/firmware', upload.single('firmware'), (req, res, next) => {
+    console.log(req.file);
+    let fileUrl = '';
+    if (req.file) {
+        fileUrl = 'firmwares/' + req.file.filename;
+    }
+
+    res.status(200);
+    res.json({ file_url: fileUrl });
+});
+
+io.on('connection', (socket) => {
+    console.log('a user connected');
+});
+
 app.use(router);
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
+server.listen(PORT, () => {
     console.log(`Example app listening on port ${PORT}`);
 });
