6a18795d8af43abc02555aaf9f646c1ab22767f2
doanduchieupw <doanduchieu.pw12@gmail.com>
2022-09-05 22:23:44 +0700

    first commit

--- index.js
+++ index.js
@@ -8,10 +8,9 @@ router.get('/', (req, res) => {
     res.send('Hello World!');
 });
 
-app.use('/.netlify/functions/api',router);
+app.use(router);
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`);
 });
 
-module.exports.handler = serverless(app);
